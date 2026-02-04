131cfaf6f969a04f4a64c2f15ee6b5f9e4ea7ea5
royeldar <royeldar@hotmail.com>
2026-02-04 21:05:42 +0200

    implement heroface animation at menu screen

--- src/menu.c
+++ src/menu.c
@@ -9,9 +9,13 @@
 #include "keyboard.h"
 #include "menu.h"
 #include "music.h"
+#include "random.h"
 #include "scene.h"
 #include "shared_state.h"
 #include "sound.h"
+
+static unsigned int heroface_animation_ticks = 0;
+static int heroface_animation_frame = 0;
 
 /**
  * @brief Initialize the menu scene
@@ -20,14 +24,48 @@ void initialize_menu() {
     play_music("bg_menu.ogg");
 }
 
+static void update_heroface_animation() {
+    heroface_animation_ticks++;
+    if (rand_custom() % 198 == 1) {
+        if (++heroface_animation_frame == 3)
+            heroface_animation_frame = 0;
+    }
+}
+
 /**
  * @brief Update the menu scene one tick
  */
 void update_menu() {
+    update_heroface_animation();
     if (is_key_pressed(ALLEGRO_KEY_ESCAPE)) {
         play_sound("menu_change.ogg");
         g_scene = EXIT_SCENE;
     }
+}
+
+static void draw_heroface_animation() {
+    ALLEGRO_BITMAP *heroface = NULL;
+    float w, h, dx, dy, angle;
+    switch (heroface_animation_frame) {
+    case 0:
+        heroface = get_gfx_bitmap("heroface000.bmp");
+        break;
+    case 1:
+        heroface = get_gfx_bitmap("heroface001.bmp");
+        break;
+    case 2:
+        heroface = get_gfx_bitmap("heroface002.bmp");
+        break;
+    }
+    assert(heroface != NULL);
+    w = al_get_bitmap_width(heroface);
+    h = al_get_bitmap_height(heroface);
+    dx = (w / 2) + 40;
+    dy = (h / 2) + 10 + al_fixtoi(10 * al_fixsin(al_itofix(5 * heroface_animation_ticks)));
+    angle = al_fixtof(al_fixmul(
+        5 * al_fixsin(al_itofix(3 * heroface_animation_ticks) + (al_fixed)10),
+        al_fixtorad_r));
+    al_draw_rotated_bitmap(heroface, w / 2, h / 2, dx, dy, angle, 0);
 }
 
 /**
@@ -40,4 +78,5 @@ void draw_menu(const struct shared_state *shared_state) {
     ALLEGRO_BITMAP *title = get_gfx_bitmap("title.bmp");
     al_draw_bitmap(title_bg, 0, 0, 0);
     al_draw_bitmap(title, 250, 20, 0);
+    draw_heroface_animation();
 }
