48a07170a348be8437d2f12cd9b5588d65580922
royeldar <royeldar@hotmail.com>
2026-07-19 13:07:21 +0300

    draw walls

--- src/gameplay.c
+++ src/gameplay.c
@@ -35,9 +35,20 @@ static void draw_background(const struct shared_state *shared_state) {
     }
 }
 
+static void draw_walls(const struct shared_state *shared_state) {
+    ALLEGRO_BITMAP *sideblock = get_gfx_bitmap("sideblock.bmp");
+    int screen_y = shared_state->screen_y;
+    int i;
+    for (i = 0; i < 5; ++i) {
+        al_draw_bitmap(sideblock, 565, (int)((screen_y % 84) * 1.476) + (i - 1) * 124, 0);
+        al_draw_bitmap(sideblock, -57, (int)((screen_y % 84) * 1.476) + (i - 1) * 124, ALLEGRO_FLIP_HORIZONTAL);
+    }
+}
+
 /**
  * @brief Draw the gameplay scene
  */
 void draw_gameplay(const struct shared_state *shared_state) {
     draw_background(shared_state);
+    draw_walls(shared_state);
 }
